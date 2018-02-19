3aba35b372ecd3554b3271ebac9904ed81c1a609
Ubuntu <ubuntu@ip-10-0-0-6.ec2.internal>
2018-02-19 18:05:32 +0000

    change trade form submission to ajax

--- App/app.py
+++ App/app.py
@@ -44,61 +44,56 @@ def currencies():
 @app.route('/holdings/<int:user_id>', methods=['GET'])
 def holdings(user_id):
     rjson = []
-    total = 0
-    for row in session.query(User).filter_by(id=user_id).one().holdings:
-	value = prices[row.currency.code]["USD"] * row.quantity
-	rjson.append({'currency_id': row.currency.id, 'code' : row.currency.code, 'quantity' : row.quantity, 'value': value})	
-	total += value 
 
-    #rjson.append({'currency_id': 'TOTAL', 'code' : 'TOTAL', 'quantity' : '-', 'value': total})	
-    js = json.dumps(rjson)
+    user = session.query(User).filter_by(id=user_id).scalar()
+    if user == None:
+        js = json.dumps(rjson)
+        return Response(json.dumps({'error':'no user with id ' + str(user_id)}), status=400, mimetype='application/json')
+    
+    holdings = user.holdings
+    if len(holdings) == 0:
+        js = json.dumps(rjson)
+        return Response('', status=204, mimetype='application/json')
 
-    resp = Response(js, status=200, mimetype='application/json')
+    for row in holdings:
+        value = prices[row.currency.code]["USD"] * row.quantity
+        rjson.append({'currency_id': row.currency.id, 'code' : row.currency.code, 'quantity' : row.quantity, 'value': value})	
+
+    resp = Response(json.dumps(rjson), status=200, mimetype='application/json')
 
     return resp
 
 @app.route('/trades', methods=['POST'])
 def trade():
+
     rjson = {'quantity' : 0, 'my_currency_id' : -1, 'trade_currency_id' : -1}
-    if request.method == 'POST':
-    	user_id = int(request.form['user_id'])
-        my_currency_id = int(request.form['my_currency_id'])
-        trade_currency_id = int(request.form['trade_currency_id'])
-	quantity = float(request.form['quantity'])
-	user = session.query(User).filter_by(id=user_id).first()
-	my_currency_code = session.query(Currency).filter_by(id=my_currency_id).first().code
-	trade_currency_code = session.query(Currency).filter_by(id=trade_currency_id).first().code 
-	
-	if trade_currency_code == "BTC":
-	    if my_currency_code != "USD":
-	        status=403
-                rjson['error']="Cannot trade " + str(my_currency_code) + " for " + str(trade_currency_code)
-		js = json.dumps(rjson)
-		return Response(js, status=status,mimetype='application/json')
-	if trade_currency_code == "USD" or trade_currency_code == "LTC" or trade_currency_code == "DOGE" or trade_currency_code == "XMR":
-	    if my_currency_code != "BTC":
-	        status = 403
-                rjson['error']="Cannot trade " + str(my_currency_code) + " for " + str(trade_currency_code)
-		js = json.dumps(rjson)
-		return Response(js, status=status,mimetype='application/json')
-	
-    	my_currency_holdings = session.query(Holding).filter_by(currency_id=my_currency_id,user_id=user_id).first().quantity
-	if  prices[trade_currency_code][my_currency_code] * quantity <= my_currency_holdings:
-	    rjson['my_currency_holdings'] = my_currency_holdings
-	    rjson['quantity'] = quantity
-	    rjson['trade_currency_id'] = trade_currency_id
-	    rjson['my_currency_id'] = my_currency_id
-	    rjson['price'] = prices[trade_currency_code][my_currency_code] * quantity
-	    status = 200
-	else:
-	    status = 405
-            rjson['error']="Unsufficient funds for trade"
 
-    js = json.dumps(rjson)
+    user_id = 2 #int(request.form['user_id'])
+    my_currency_id = int(request.form['my_currency_id'])
+    trade_currency_id = int(request.form['trade_currency_id'])
+    quantity = float(request.form['quantity'])
+    user = session.query(User).filter_by(id=user_id).first()
+    my_currency_code = session.query(Currency).filter_by(id=my_currency_id).first().code
+    trade_currency_code = session.query(Currency).filter_by(id=trade_currency_id).first().code 
 
-    resp = Response(js, status=status, mimetype='application/json')
+    if trade_currency_code == 'BTC':
+        if my_currency_code != 'USD':
+            return Response(json.dumps({'error' : 'Cannot trade ' + str(my_currency_code) + ' for ' + str(trade_currency_code)}), status=403, mimetype='application/json')
+    if trade_currency_code == 'USD' or trade_currency_code == 'LTC' or trade_currency_code == 'DOGE' or trade_currency_code == 'XMR':
+        if my_currency_code != 'BTC':
+            return Response(json.dumps({'error' : 'Cannot trade ' + str(my_currency_code) + ' for ' + str(trade_currency_code)}), status=403, mimetype='application/json')
 
-    return resp
+    my_currency_holdings = session.query(Holding).filter_by(currency_id=my_currency_id,user_id=user_id).first().quantity
+    if  prices[trade_currency_code][my_currency_code] * quantity <= my_currency_holdings:
+        rjson['my_currency_holdings'] = my_currency_holdings
+        rjson['quantity'] = quantity
+        rjson['trade_currency_id'] = trade_currency_id
+        rjson['my_currency_id'] = my_currency_id
+        rjson['price'] = prices[trade_currency_code][my_currency_code] * quantity
+        return Response(json.dumps(rjson), status=200 , mimetype='application/json')
+    else:
+        return Response(json.dumps({'error' : 'Insufficient funds'}), status=403, mimetype='application/json')
+
 
 @app.route('/holdings')
 def showHoldings():
